3810b8ba17666e20e1fdff1b090ce9d30df502af
honjow <honjow311@gmail.com>
2024-02-05 06:38:55 +0800

    update hhd, remove ayaled

---
+++ update-module.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+module_name=$1
+
+
+# if module_name is empty, update all modules
+if [ -z "$module_name" ]; then
+    git submodule update --remote --recursive 
+    exit 0
+fi
+
+module_path="aur-pkgs/$module_name"
+git submodule update --remote "$module_path"
