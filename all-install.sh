ffe6c3f6777c484cd4e4c380117915dee029dd96
honjow <honjow311@gmail.com>
2024-03-15 11:16:14 +0800

    Btrfs compression

--- all-install.sh
+++ all-install.sh
@@ -3,8 +3,6 @@
 set -e
 set -x
 
-source /manifest
-
 pacman-key --populate
 
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -103,10 +101,10 @@ Subsystem	sftp	/usr/lib/ssh/sftp-server
 " > /etc/ssh/sshd_config
 
 echo "
-LABEL=frzr_root /          btrfs subvol=deployments/${SYSTEM_NAME}-${VERSION},ro,noatime,nodatacow 0 0
-LABEL=frzr_root /var       btrfs subvol=var,rw,noatime,nodatacow 0 0
-LABEL=frzr_root /home      btrfs subvol=home,rw,noatime,nodatacow 0 0
-LABEL=frzr_root /frzr_root btrfs subvol=/,rw,noatime,nodatacow 0 0
+LABEL=frzr_root /          btrfs subvol=deployments/${SYSTEM_NAME}-${VERSION},ro,noatime,compress=zstd:3 0 0
+LABEL=frzr_root /var       btrfs subvol=var,rw,noatime 0 0
+LABEL=frzr_root /home      btrfs subvol=home,rw,noatime 0 0
+LABEL=frzr_root /frzr_root btrfs subvol=/,rw,noatime 0 0
 LABEL=frzr_efi  /boot      vfat  rw,noatime,nofail  0 0
 " > /etc/fstab
 
