8fc46e7e22a52f85a6f98694f0b40a899ce6a282
honjow <honjow311@gmail.com>
2024-03-15 10:57:47 +0800

    update hhd

---
+++ clear-moudle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+rm -rf "aur-pkgs"
+git checkout "aur-pkgs"
--- update-module.sh
+++ update-module.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 module_name=$1
-
+set -e
 
 # if module_name is empty, update all modules
 if [ -z "$module_name" ]; then
-    git submodule update --remote --recursive 
+    git submodule update --remote --recursive
     exit 0
 fi
 
