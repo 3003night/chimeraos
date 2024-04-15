0b1e1ef2a79fdbee74d6e5e47d5ef9ce3e280f41
honjow <honjow311@gmail.com>
2024-04-15 19:52:33 +0800

    exit when build failed

--- pkgs/build-package.sh
+++ pkgs/build-package.sh
@@ -7,5 +7,9 @@ sudo chown -R build:build /workdir/pkgs
 PIKAUR_CMD="PKGDEST=/workdir/pkgs pikaur --noconfirm --build-gpgdir /etc/pacman.d/gnupg -S -P /workdir/${1}/PKGBUILD"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
+if [ $? -ne 0 ]; then
+    echo "Build failed. Stopping..."
+    exit -1
+fi
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
 find /workdir/pkgs/*.pkg.tar* -type f -name '*:*' -execdir bash -c 'mv "$1" "${1//:/--}"' bash {} \;
